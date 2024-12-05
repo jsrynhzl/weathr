f89a50b07298393c0c8be23bb34e6db2b3da48e8
sally <wykee001@gmail.com>
2024-12-05 16:58:12 -0500

    home_commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,16 @@
-import { View, Image, SafeAreaView, TextInput } from 'react-native'
+import { View, Image, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { theme } from '../theme'
 
+import { MagnifyingGlassIcon} from 'react-native-heroicons/outline'
+
+import { MapPinIcon } from 'react-native-heroicons/solid'
+
 const HomeScreen = () => {
-    return(
+    const [showSearch, toggleSearch] = useState(false);
+    const [location, setLocation] = useState(1, 2, 3);
+    return (
         <View className="flex-1 relative">
             <StatusBar style="light" />
             <Image
@@ -14,18 +20,49 @@ const HomeScreen = () => {
             />
             <SafeAreaView className="flex flex-1">
                 {/* Search section */}
-                <View style={{height: '7%'}} className="mx-4 relative z-50">
+                <View style={{ height: '7%' }} className="mx-4 relative z-50">
                     <View
                         className="flex-row justify-end items-center rounded-full"
-                        style={{backgroundColor: theme.bgWhite(0.2)}}>
-                            <TextInput
-                                placeholder="Search city"
-                                placeholderTextColor={'lightgray'}
-                                className="pl-6 h-10 flex-1 text-base text-white"
-                            />
+                        style={{ backgroundColor: showSearch ? theme.bgWhite(0.2) : 'transparent' }}>
+                        {
+                            showSearch ? (
+                                <TextInput
+                                    placeholder="Search city"
+                                    placeholderTextColor={'lightgray'}
+                                    className="pl-6 h-10 pb-1 flex-1 text-base text-white"
+                                />
+                            ) : null
+                        }
+                        <TouchableOpacity
+                            onPress={() => toggleSearch(!showSearch)}
+                            style={{ backgroundColor: theme.bgWhite(0.3) }}
+                            className="rounded-full p-3 m-1"
+                        >
+                            <MagnifyingGlassIcon size="25" color="white" />
+                        </TouchableOpacity>
                     </View>
+                    {
+                        locations.length > 0 && showSearch ? (
+                            <View className='absolute w-full bg-gray-300 top-16 rounded-3xl'>
+                                {
+                                    locations.map((loc, index) => {
+                                        let showBorder = index+1 != locations.length;
+                                        let borderClass = showBorder ? 'border-b-2 border-b-gray-400' : '';
+                                        return (
+                                            <TouchableOpacity
+                                                key={index}
+                                                className={"flex-row items-center border-0 p-3 px-4 mb-1" +borderClass}
+                                            >
+                                                <MapPinIcon size="20" color="gray" />
+                                                <Text>Toronto, Canada</Text>
+                                            </TouchableOpacity>
+                                        )
+                                    })
+                                }
+                            </View>
+                        ) : null
+                    }
                 </View>
-
             </SafeAreaView>
         </View>
     )
